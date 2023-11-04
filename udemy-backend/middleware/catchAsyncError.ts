1bf1bb5913acfc8b38cdbd30fc24d6d783c924c9
Wajid Ali Altamash <68860591+wajid90@users.noreply.github.com>
2023-11-04 05:57:09 -0400

    Delete udemy-backend directory

--- udemy-backend/middleware/catchAsyncError.ts
+++
@@ -1,5 +0,0 @@
-import { Request,Response,NextFunction } from "express";
-
-export const catchAsyncError = (isFunc:any) =>(req:Request, res:Response, next:NextFunction) => Promise.resolve(isFunc(req,res,next)).catch(next);
-
-// export default func => (req:Request, res:Response, next:NextFunction) => Promise.resolve(func(req, res, next)).catch(next);
